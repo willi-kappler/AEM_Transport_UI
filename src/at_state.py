64e6f8595f249f365d93dabe43efff07d61c85c3
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-11-07 17:29:44 +0100

    Add first code for check_session.

--- src/at_state.py
+++ src/at_state.py
@@ -1,12 +1,17 @@
 
 # Python imports
+import logging
 import sqlite3
+import uuid
 
 # External imports
 
 
 # Local imports
 import at_config
+
+
+logger = logging.getLogger(__name__)
 
 
 class ATMainState:
@@ -19,7 +24,30 @@ class ATMainState:
     def activate(self):
         self.db = sqlite3.connect(self.configuration.db_filename)
 
-    def check_session(self, cookies: dict[str, str]) -> bool:
-        return False
+    def check_session(self, cookies: dict[str, str], client_host) -> bool:
+        if "session_id" in cookies:
+            session_id = cookies["session_id"]
+
+            try:
+                uuid_object = uuid.UUID(session_id, version=4)
+            except Exception:
+                logging.debug(f"Invalid session id (uuid version 4): {session_id}")
+                return False
+
+            cursor = self.db.cursor()
+            db_res = cursor.execute(f"SELECT ip_hash FROM sessions WHERE uuid = '{session_id}'")
+            db_row = db_res.fetchone()
+            if db_row is None:
+                return False
+
+            # TODO: check for ip or sth. else...
+
+            return True
+        else:
+            return False
+
+    def get_current_user(self) -> str:
+        return "dummy_user"
+
 
 
