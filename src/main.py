ecce48d98689dad3a08417c2f46105b21cc4eec5
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-11-21 15:14:45 +0100

    Use lifespan to initialize global state.

--- src/main.py
+++ src/main.py
@@ -4,10 +4,11 @@
 import logging
 import sys
 from typing import Optional
+from contextlib import asynccontextmanager
 
 # External imports
 import uvicorn
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -19,11 +20,36 @@ import at_config
 
 logger = logging.getLogger(__name__)
 
-app = FastAPI()
+@asynccontextmanager
+async def init_data(app: FastAPI):
+    global main_state
+
+    print("Lifespan: init_data()")
+    global_config = at_config.ATConfiguration()
+
+    all_args = sys.argv
+    num_of_args = len(all_args)
+
+    if num_of_args == 1:
+        logging.info("Using defualt configuration.")
+    elif num_of_args == 2:
+        config_filename = all_args[1]
+        global_config.from_file(config_filename)
+    else:
+        logging.error(f"Got too many command line arguments: {all_args}")
+        raise ValueError("Only expected one command line argument: filename of configuration file.")
+
+    main_state = at_state.ATMainState()
+    main_state.set_config(global_config)
+    main_state.activate()
+
+    yield
+
+
+app = FastAPI(lifespan=init_data)
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
-main_state = at_state.ATMainState()
 
 
 @app.get("/")
@@ -45,41 +71,25 @@ async def login_get(request: Request):
     match maybe_user:
         case None:
             return templates.TemplateResponse(name="login.html",
-                context={"request": request})
+                context={"request": request, "failed_login": False})
         case _:
             return RedirectResponse(url="/")
 
 
 @app.post("/login")
-async def login_post(request: Request, username: str, password: str):
-    if main_state.create_new_session(username, password):
+async def login_post(request: Request, login: str = Form(), passwd: str = Form()):
+    if main_state.create_new_session(login, passwd):
         return RedirectResponse(url="/")
     else:
         return templates.TemplateResponse(name="login.html",
             context={"request": request, "failed_login": True})
+
 
 if __name__ == "__main__":
     log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     log_file_name = "server.log"
     logging.basicConfig(filename=log_file_name, level=logging.DEBUG, format=log_format)
     logging.getLogger("asyncio").setLevel(logging.WARNING)
-
-    global_config = at_config.ATConfiguration()
-
-    all_args = sys.argv
-    num_of_args = len(all_args)
-
-    if num_of_args == 1:
-        logging.info("Using defualt configuration.")
-    elif num_of_args == 2:
-        config_filename = all_args[1]
-        global_config.from_file(config_filename)
-    else:
-        logging.error(f"Got too many command line arguments: {all_args}")
-        raise ValueError("Only expected one command line argument: filename of configuration file.")
-
-    main_state.set_config(global_config)
-    main_state.activate()
 
     uv_config = uvicorn.Config("main:app", port=5000, log_level="debug")
     server = uvicorn.Server(uv_config)
