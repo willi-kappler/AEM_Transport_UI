593819c1cbf4579a1e68ead076f2aa59e01fd6bb
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-11-22 17:01:16 +0100

    Set session cookie and add logout function.

--- src/main.py
+++ src/main.py
@@ -53,6 +53,7 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
+@app.post("/")
 async def main_page(request: Request):
     maybe_user: Optional[str] = main_state.get_current_user(request)
 
@@ -71,25 +72,48 @@ async def login_get(request: Request):
     match maybe_user:
         case None:
             return templates.TemplateResponse(name="login.html",
-                context={"request": request, "failed_login": False})
+                context={"request": request, "message": "Please log in first."})
         case _:
             return RedirectResponse(url="/")
 
 
 @app.post("/login")
 async def login_post(request: Request, login: str = Form(), passwd: str = Form()):
-    if main_state.create_new_session(login, passwd):
-        return RedirectResponse(url="/")
-    else:
-        return templates.TemplateResponse(name="login.html",
-            context={"request": request, "failed_login": True})
+    maybe_session = main_state.create_new_session(login, passwd)
+
+    match maybe_session:
+        case None:
+            return templates.TemplateResponse(name="login.html",
+                context={"request": request, "message": "Failed login, please retry!"})
+        case new_session:
+            response = RedirectResponse(url="/")
+            response.set_cookie(key=at_state.AT_SESSION_ID, value=new_session, max_age=at_state.AT_SESSION_MAX_AGE)
+            return response
+
+
+@app.get("/logout")
+@app.post("/logout")
+async def logout_get(request: Request):
+    maybe_user: Optional[str] = main_state.get_current_user(request)
+
+    match maybe_user:
+        case None:
+            return RedirectResponse(url="/login")
+        case user:
+            main_state.logout_user(user)
+            response = RedirectResponse(url="/login")
+            response.delete_cookie(key=at_state.AT_SESSION_ID)
+            return response
 
 
 if __name__ == "__main__":
     log_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
     log_file_name = "server.log"
     logging.basicConfig(filename=log_file_name, level=logging.DEBUG, format=log_format)
+
+    # Silence loggers from other modules:
     logging.getLogger("asyncio").setLevel(logging.WARNING)
+    logging.getLogger("multipart").setLevel(logging.WARNING)
 
     uv_config = uvicorn.Config("main:app", port=5000, log_level="debug")
     server = uvicorn.Server(uv_config)
