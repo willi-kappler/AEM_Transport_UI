a6d703a37b09f8f7b9d2bb8fec029e29008ca7aa
Willi Kappler <willi.kappler.gm@gmail.com>
2024-12-18 22:12:07 +0100

    Finish login form.

--- src/at_login.py
+++ src/at_login.py
@@ -1,7 +1,7 @@
 
 
 # External imports
-from nicegui import ui
+from nicegui import app, ui
 
 # Local imports
 import at_state
@@ -10,15 +10,17 @@ import at_state
 
 class ATLoginUI:
     def __init__(self, main_state: at_state.ATMainState):
-        self.main_state = main_state
+        self.main_state: at_state.ATMainState = main_state
 
         with ui.dialog() as error_dialog:
-            ui.label("Login failed!").classes("text-2xl text-red-500 font-bold")
+            with ui.card():
+                ui.label("Login failed!").classes("text-2xl text-red-500 font-bold")
 
         self.error_dialog = error_dialog
 
     def check_login(self):
-        if self.main_state.check_login(self.login_id.value, self.password.value):
+        browser_id: str = app.storage.browser["id"]
+        if self.main_state.check_login(self.username.value, self.password.value, browser_id):
             ui.navigate.to("/main")
         else:
             self.error_dialog.open()
@@ -27,8 +29,8 @@ class ATLoginUI:
         with ui.card().classes("absolute-center items-center"):
             ui.label("AEM Transport").classes("text-2xl font-bold")
             ui.label("Please log in first:").classes("text-xl")
-            self.login_id = ui.input(label="Login ID").bind_value_to(self, "login_id")
-            self.password = ui.input(label="Password", password=True).bind_value_to(self, "password")
+            self.username = ui.input(label="Username")
+            self.password = ui.input(label="Password", password=True)
             ui.button("Login", icon="login", on_click=self.check_login).classes("rounded-lg")
 
 
