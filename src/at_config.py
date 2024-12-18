a0519c14d9d916a64e1fa4a48fe11eca8ef69757
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-12-18 17:26:37 +0100

    Add secret for client cookie.

--- src/at_config.py
+++ src/at_config.py
@@ -11,6 +11,7 @@ class ATConfiguration:
     def __init__(self):
         self.db_filename = "at_transport.db"
         self.port = 5000
+        self.secret = "52654762f3a77d6c788b8b77322b771e4291c604c2fa244fef6e8b6247ba6d8d"
 
     def from_file(self, filename: str):
         logger.debug(f"Load configuration from file: {filename}.")
@@ -25,6 +26,6 @@ class ATConfiguration:
             self.port = int(data["port"])
             assert (self.port >= 0 and self.port <= 65535), f"Port must be in range 0-65535: {self.port}"
 
-
-
+        if "secret" in data:
+            self.secret = data["secret"]
 
