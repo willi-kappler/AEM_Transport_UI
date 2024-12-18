60a0c28d571d3884497a05b48dbd48f085128eea
Willi Kappler <willi.kappler@uni-tuebingen.de>
2024-12-18 17:25:15 +0100

    Update dependencies.

--- shell.nix
+++ shell.nix
@@ -3,17 +3,12 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [
       (python312.withPackages(ps: with ps; [
-        fastapi
         flake8
-        ipython
-        jinja2
-        python-multipart
         mypy
-        uvicorn
-        pydantic
         ]))
       pyright
       ruff
+      uv
     ];
 }
 
